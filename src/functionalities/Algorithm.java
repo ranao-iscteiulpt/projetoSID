1be08db8b71e8a218e824ff8a146a0537520e97a
njtba-iscteiul <njtba@iscte-iul.pt>
2018-05-10 10:24:30 +0100

    Algorithm

--- src/functionalities/Algorithm.java
+++ src/functionalities/Algorithm.java
@@ -1,18 +1,19 @@
 package functionalities;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-public class Write {
+public class Algorithm {
+
+	public ArrayList<JSONObject> json = new ArrayList<>();
 	
-	private File file = new File("messagemSensor.json");
-	
-	public void dateTime(MqttMessage message){
+	public void insertDateTime(MqttMessage message){
 	
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		Date date = new Date();
@@ -21,49 +22,22 @@ public class Write {
 		
 		String messageString = "{" + message.toString() + ",\"date\":\"" + dateFormat.format(date) + "\",\"time\":\"" + timeFormat.format(time) + "\"}";
 		
-		writeToJSON(messageString);
+		insertToJSONArray(messageString);
 	}
 
-	private void writeToJSON(String messageString) {
-		
-		FileWriter fileWriter = null;
-		
-		if(!file.exists()){
-			try {
-				file.createNewFile();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-						
-		try {
-			
-			fileWriter = new FileWriter(file, true);
-			
-			fileWriter.append(messageString + "\n");
-			fileWriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		/*JSONParser parser = new JSONParser();
+	public void insertToJSONArray(String messageString) {
 		
 		try {
-			Object obj = parser.parse(new FileReader(file));
-			JSONObject jsonObject = (JSONObject) obj;
-			String s = (String) jsonObject.get("temperature");
-			System.out.println(s);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) parser.parse(messageString);
+			json.add(jsonObject);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
+	}
+	
+	public ArrayList<JSONObject> getJSONArray(){
+		return json;
 	}
 }
